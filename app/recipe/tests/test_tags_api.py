c0649d2abbefa1f89bdeabef66ee3c4463deb53d
Aleksandr Kiselev <a.kiselev@brandquad.ru>
2022-07-14 21:10:40 +0300

    Add tags delete

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -84,3 +84,14 @@ class PrivateTagsApiTests(TestCase):
         tag.refresh_from_db()
 
         self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting tag."""
+        tag = Tag.objects.create(user=self.user, name='After dinner')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
